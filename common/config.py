f573673ad066add906dd3d8ce3adab55257d5997
HuangLK <huanglk3@gmail.com>
2014-05-19 22:35:06 +0800

    issue version

--- common/config.py
+++ common/config.py
@@ -1,11 +1,13 @@
+import sae
+
 ## DataBase
 DB_DIALECT = 'mysql'
 DB_DRIVER = ''
-DB_USERNAME = ''
-DB_PASSWORD = ''
-DB_HOST = 'localhost'
-DB_PORT = '3306'
-DB_DATABASE = 'test'
+DB_USERNAME = sae.const.MYSQL_USER
+DB_PASSWORD = sae.const.MYSQL_PASS
+DB_HOST = sae.const.MYSQL_HOST
+DB_PORT = sae.const.MYSQL_PORT
+DB_DATABASE = sae.const.MYSQL_DB
 DB_QUERYSTRING = '?charset=utf8'
 
 SESSION_KEY = 'vkFwRjPgASwNed1sBmXjy+o/OSY0qFsPe1fljeyDE5aSbidWesAp9tNcenWcnmnS'
